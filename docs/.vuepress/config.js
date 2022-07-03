b63349c0fcb6f15f0d8765637e63eb7e4b33b3db
Jihun Kim <zlhunzzing@gmail.com>
2022-07-03 16:59:51 +0900

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "김지훈 블로그",
+  base: "/blog/",
+  themeConfig: {
+    // nav: [{ text: "", link: "/guestbook" }],
+    sidebar: [
+      {
+        title: "Test",
+        children: ["/test/"],
+      },
+    ],
+  },
+};
---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <ParentLayout>
+    <Comment  slot="page-bottom"/>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
+
+export default {
+  components: {
+    ParentLayout
+  }
+};
+</script>
