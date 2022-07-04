e560baa19a1a3cd554f15ad9989121def9db0c30
Jihun Kim <zlhunzzing@gmail.com>
2022-07-04 15:43:23 +0900

    feat: Intro page

---
+++ docs/.vuepress/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <ParentLayout>
+    <div class="content" slot="page-bottom">
+      <h1>FE 김지훈 블로그</h1>
+      <div>Intro</div>
+    </div>
+  </ParentLayout>
+</template>
+
+<style scoped>
+  .content {
+    max-width: 740px;
+    margin: 0 auto;
+    padding: 2rem;
+    padding-top: 0px;
+  }
+  
+  h1 {
+    margin-top: 0;
+  }
+</style>
+
+<script>
+import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
+
+export default {
+  components: {
+    ParentLayout
+  }
+};
+</script>
