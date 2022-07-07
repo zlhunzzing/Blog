d5f010399a5102b08dc75c235f6e3b2296f8a7e4
Jihun Kim <zlhunzzing@gmail.com>
2022-07-07 17:45:05 +0900

    refactor: build sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,17 @@
+const fs = require("fs");
+
+const routes = ["html", "javascript"];
+
+const sidebar = routes.map((route) => ({
+  title: route,
+  children: fs.readdirSync("./docs/" + route).map((path) => route + "/" + path),
+}));
+
 module.exports = {
-  title: "김지훈 블로그",
+  title: "김지훈 FE 블로그",
   base: "/blog/",
   themeConfig: {
     nav: [{ text: "방명록", link: "/guestbook/" }],
-    sidebar: [
-      {
-        title: "Test",
-        children: ["/test/"],
-      },
-    ],
+    sidebar,
   },
 };
---
+++ docs/javascript/zsczsc.md
@@ -0,0 +1,5 @@
+# hzzsczscs
+
+TEst
+
+<Comment />
